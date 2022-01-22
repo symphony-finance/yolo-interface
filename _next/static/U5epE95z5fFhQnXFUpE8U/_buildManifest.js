5b05e23e396476d01a13d3c846c56aead0827f5c
Kakashi <symphfi@gmail.com>
2022-01-22 10:39:10 +0530

    changes commit

---
+++ _next/static/U5epE95z5fFhQnXFUpE8U/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/134-410b85141073a167.js","static/chunks/pages/index-a2006339543a405d.js"],"/_error":["static/chunks/pages/_error-0ff9de454a09d4a2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
