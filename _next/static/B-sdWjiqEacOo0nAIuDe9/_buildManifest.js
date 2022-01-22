150c8d592508892e08cb317aecbafbcd83533c26
Kakashi <symphfi@gmail.com>
2022-01-22 10:32:33 +0530

    fail issue fix

--- _next/static/B-sdWjiqEacOo0nAIuDe9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/134-410b85141073a167.js","static/chunks/pages/index-3e9d37965d2cdeb6.js"],"/_error":["static/chunks/pages/_error-0ff9de454a09d4a2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/WlelqpUu4LqtnXPF_7K58/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/134-410b85141073a167.js","static/chunks/pages/index-e516725fd47444a2.js"],"/_error":["static/chunks/pages/_error-0ff9de454a09d4a2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
